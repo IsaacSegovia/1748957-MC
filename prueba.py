1b2602e94bc802aba94b669fc992f07c1faa43d5
IsaacSegovia <30887528+IsaacSegovia@users.noreply.github.com>
2017-08-13 23:23:14 -0500

    Add files via upload
    Prueba

---
+++ prueba.py
@@ -0,0 +1,24 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("prueba")
+prueba
+>>> 1+1
+2
+>>> 1<2
+True
+>>> 1<1
+False
+>>> for i in range(1,10) :
+	print(i)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
