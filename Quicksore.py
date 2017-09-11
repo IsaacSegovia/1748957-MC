3b1d4ebf244dbb2dba3132821301cbb20efcc0da
IsaacSegovia <30887528+IsaacSegovia@users.noreply.github.com>
2017-09-10 22:43:13 -0500

    Add files via upload

---
+++ Quicksore.py
@@ -0,0 +1,32 @@
+import random 
+cnt=0
+
+def quicksort(arr):
+	global cnt
+	if len(arr) <= 1:
+		return arr
+	p=arr.pop(0)
+	menores, mayores=[], []
+	for e in arr:
+		cnt+=1 
+		if e <=p:
+			menores.append(e)
+		else:
+			mayores.append(e)
+	return quicksort(menores) +[p] + quicksort(mayores)
+
+def rndar(long):
+	arr=[]
+	for i in range(long):
+		arr.append(random, randint(0, long))
+	return arr
+
+l=10
+
+while 1<=10
+	for replica in range(10)
+		ori=rndar(1)
+		arr=quicksort(ori)
+		print(1, cnt, arr, ori)
+		cnt=0
+	l*=2
