e722c02f67ed2c9fbd851f1e300906efd316f0e2
IsaacSegovia <30887528+IsaacSegovia@users.noreply.github.com>
2017-09-11 20:19:40 -0500

    Add files via upload

--- Inserction.py
+++ Inserction.py
@@ -1,19 +1,17 @@
 #Insercion
 
-def orden_por_insercion(array):
-	global cnt
-	for indice in range (1, len(array)):
-		valor=array[indice] #valor es el elemento que vamos a comparar
-		i=indice-1          #i es el valor anterior al elemento que estamos comparando
-		while i>=0:
-			cnt=cnt+1
-			if valor<array[i]:  #comparamos valor con el elemtno anterior
-				array[i+1]=array[i]  #Intercambiamos los valores
-				array[i]=valor
-				i-=1 #Decrementamos en 1 el valor de i
-			else:
-				break
-	return array
-A=[26,2,45,30,1,450,1]
 cnt=0
-orden_por_insercion(A)
+def orden_por_insercion(array):
+    global cnt
+    for indice in range (1,len(array)):
+        valor=array[indice]
+        i=indice-1
+        while i>=0:
+            cnt+=1
+            if valor<array[i]:
+                array[i+1]=array[i]
+                array[i]=valor
+                i-=1
+            else:
+                break
+    return array
